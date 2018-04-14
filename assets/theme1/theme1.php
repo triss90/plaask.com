84538efe186a1eb50622aa2e92690932402f31cc
Tristan White <oliver.tristan@gmail.com>
2018-04-14 16:08:42 +0200

    Adding theme folder and start of first theme design

---
+++ assets/theme1/theme1.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="../css/beagle.css">
+</head>
+
+ 
+<style>
+    <?php include_once("theme1.css"); ?>
+</style>
+
+<body>
+
+    <header class="t1-header">
+        <div class="container">
+            <div class="row center-tiny">
+                <div class="tiny-12 small-6 ">
+                    <h1 id="t1-header-title" contenteditable="true" class="contenteditable t1-header-title">I'm Jonathan Doe.</h1>
+                </div>
+            </div>
+            <div class="row center-tiny">
+                <div class="tiny-12 small-8">
+                    <p id="t1-header-tagline" contenteditable="true" class="contenteditable t1-header-tagline">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="t1-about">
+        <div class="container">
+            <div class="row">
+                <div class="tiny-12 small-4 medium-3 large-3">
+                    <img id="t1-about-image" contenteditable="true" class="contenteditable t1-about-image" src="/assets/theme1/avatar.png" alt="Profile Picture of Jonathan Doe">
+                </div>
+                <div class="tiny-12 small-8 medium-9 large-9">
+                    <h2 id="t1-about-title" contenteditable="true" class="contenteditable t1-about-title">About me</h2>
+                    <p id="t1-about-text" contenteditable="true" class="contenteditable t1-about-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
+                    <div class="row">
+                        <div class="tiny-12 small-6">
+                            <h2 id="t1-contact-title" contenteditable="true" class="contenteditable t1-contact-title">Contact Details</h2>
+                            <p id="t1-contact-text" contenteditable="true" class="contenteditable t1-contact-text">
+                                Jonathan Doe <br>
+                                1600 Amphitheatre Parkway <br>
+                                Mountain View, CA 94043 US <br>
+                                (123)456-7890 <br>
+                                anyone@website.com
+                            </p>
+                        </div>
+                        <div class="tiny-12 small-6">
+                            <button id="t1-download-resume-button" contenteditable="true" class="contenteditable t1-download-resume-button">Download Resume</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="t1-resume">
+        <div class="container">
+            <div class="row">
+                <div class="tiny-12 small-4 medium-3 large-3">
+                    <h2 id="t1-resume-title-1" contenteditable="true" class="contenteditable t1-resume-title-1">Education</h2>
+                </div>
+                <div class="tiny-12 small-8 medium-9 large-8">
+                    <div id="t1-resume-text-1" contenteditable="true" class="contenteditable t1-resume-text-1">
+                        <h3>University of Life</h3>
+                        <i><strong>Master in Graphic Design • April 2007</strong></i>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.</p>
+                        <br>
+                        <h3><strong>School of Cool Designers</h3>
+                        <i>B.A. Degree in Graphic Design • March 2003</strong></i>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.</p>
+                    </div>
+                 </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="tiny-12 small-4 medium-3 large-3">
+                    <h2 id="t1-resume-title-2" contenteditable="true" class="contenteditable t1-resume-title-2">work</h2>
+                </div>
+                <div class="tiny-12 small-8 medium-9 large-8">
+                    <div id="t1-resume-text-2" contenteditable="true" class="contenteditable t1-resume-text-2">
+                        <h3>Awesome Design Studio</h3>
+                        <i><strong>Senior UX Designer • March 2010 - Present</strong></i>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.</p>
+                        <br>
+                        <h3>Super Cool Studio</h3>
+                        <i><strong>UX Designer • March 2007 - February 2010</strong></i>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.</p>
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/js/medium-editor.min.js"></script>
+    <script src="../js/plaask-editor-min.js"></script>
+
+    <script>var editor = new MediumEditor('.contenteditable');</script>
+
+</body>
+</html>
+
